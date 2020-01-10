12074cdd116a62114498e82beee59e3408b1fb3f
Bill Mills <mills.wj@gmail.com>
2020-01-10 11:19:54 -0500

    Update api-spec.js

--- api/integration/api-spec.js
+++ api/integration/api-spec.js
@@ -2,7 +2,7 @@ var request = require('request');
 var base_url = "http://apitest:8080/";    
 
 describe("When testing 'api/products/1'", function(){
-    it("should respond with the URL of a cat GIF", function(done) {
+    it("should respond with entry for 'resistor'", function(done) {
         request(base_url + 'api/products/1', function(error, response, body){
             product = JSON.parse(body)
             expect(product["description"]).toMatch('resistor');
